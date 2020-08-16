0e9fdeb1a54ed479bbede351684e4f41e0de4abe
Jan-Marvin <69450869+Jan-Marvin@users.noreply.github.com>
2020-08-16 21:40:38 +0200

    Add files

---
+++ src/main/java/com/minecraft/simpleautofishing/simpleautofishing.java
@@ -0,0 +1,25 @@
+package com.minecraft.simpleautofishing;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class simpleautofishing implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "simpleautofishing";
+    public static final String MOD_NAME = "simpleautofishing";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+    }
+
+    public static void log(Level level, String message) {
+        LOGGER.log(level, "[" + MOD_NAME + "] " + message);
+    }
+
+}
