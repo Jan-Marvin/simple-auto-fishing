b3d11dd4210ec00eeb7268f93ac8bc63f7a889cc
Jan-Marvin <69450869+Jan-Marvin@users.noreply.github.com>
2020-08-25 19:30:38 +0200

    Fix spacing

--- src/main/java/com/minecraft/simpleautofishing/safCore.java
+++ src/main/java/com/minecraft/simpleautofishing/safCore.java
@@ -16,81 +16,81 @@ public class safCore {
 	int timer;
 	int lock;
 	static Minecraft Inst = Minecraft.getInstance();
-	    
-    //stuff for retract and extract timing
-    @SubscribeEvent
-    public void onPlayerTick(ClientTickEvent event) {
-    	//reset helper when bobber not exist
-    	if (!FishingBobberExist()) {
-    		timer = 0;
-    	}
-    	//check if bobber is on water
-    	if (FishingBobberExist() && Inst.player.fishingBobber.isInWater()) {
-    		if (timer == 0) {
-    			timer = 1;
-    		} else if (timer == 25) {
-    			//check if bobber is pulled down
-    			if (Inst.player.fishingBobber.getMotion().y < -0.1D) {
-    				retract = true;
-    			}
-    		} else if (timer < 25) {
-    			timer++;
-    		}
-    	}
-    	if (retract || extract) {
-    		if(timer2 == 10 && retract){
-    			if (lock == 0) {
-    				UseRod();
-    				//remove bobber
-    				if (FishingBobberExist()) {
-    					Inst.player.fishingBobber.remove(false);
-    				}
-    				timer2=0;
-    				retract = false;
-    				extract = true;
-    				lock = 1;
-    			}
-    		}
-    		if(timer2 == 30 && extract) {
-    			if (safMode.Modes()) {
-    				UseRod();
-    			}
-    			timer2=0;
-            	extract = false;
-            	lock = 0;
-    		}
-    	timer2++;
-    	}
-    } 
-    
-    //check if player exist
-    public boolean PyerExist() {
-    	if (Inst.player != null) {
-    		return true;
-    	}
-    	return false;
-    }
-    
-    //check if bobber exist
-    public boolean FishingBobberExist() {
-    	if (PyerExist() && Inst.player.fishingBobber != null) {
-    		return true;
-    	}
-    	return false;
-    }
-    
-    //hand selector
-    public static Hand getHand() {
-    	if (Inst.player.getHeldItemMainhand().getItem() == Items.FISHING_ROD) {
-    		return Hand.MAIN_HAND;
-    	} else {
-    		return Hand.OFF_HAND;
-    	}
-    }
-    
-    //use road
-    public static ActionResultType UseRod() {
-        Inst.player.swingArm(getHand());
-        return Inst.playerController.processRightClick(Inst.player, Inst.world, getHand());
-    }
+
+	//stuff for retract and extract timing
+	@SubscribeEvent
+	public void onPlayerTick(ClientTickEvent event) {
+		//reset helper when bobber not exist
+		if (!FishingBobberExist()) {
+			timer = 0;
+		}
+		//check if bobber is on water
+		if (FishingBobberExist() && Inst.player.fishingBobber.isInWater()) {
+			if (timer == 0) {
+				timer = 1;
+			} else if (timer == 25) {
+				//check if bobber is pulled down
+				if (Inst.player.fishingBobber.getMotion().y < -0.1D) {
+					retract = true;
+				}
+			} else if (timer < 25) {
+				timer++;
+			}
+		}
+		if (retract || extract) {
+			if(timer2 == 10 && retract){
+				if (lock == 0) {
+					UseRod();
+					//remove bobber
+					if (FishingBobberExist()) {
+						Inst.player.fishingBobber.remove(false);
+					}
+					timer2=0;
+					retract = false;
+					extract = true;
+					lock = 1;
+				}
+			}
+			if(timer2 == 30 && extract) {
+				if (safMode.Modes()) {
+					UseRod();
+				}
+				timer2=0;
+				extract = false;
+				lock = 0;
+			}
+		timer2++;
+		}
+	} 
+
+	//check if player exist
+	public boolean PyerExist() {
+		if (Inst.player != null) {
+			return true;
+		}
+		return false;
+}
+
+	//check if bobber exist
+	public boolean FishingBobberExist() {
+		if (PyerExist() && Inst.player.fishingBobber != null) {
+			return true;
+		}
+		return false;
+	}
+
+	//hand selector
+	public static Hand getHand() {
+		if (Inst.player.getHeldItemMainhand().getItem() == Items.FISHING_ROD) {
+			return Hand.MAIN_HAND;
+		} else {
+			return Hand.OFF_HAND;
+		}
+	}
+
+	//use road
+	public static ActionResultType UseRod() {
+		Inst.player.swingArm(getHand());
+		return Inst.playerController.processRightClick(Inst.player, Inst.world, getHand());
+	}
 }
--- src/main/java/com/minecraft/simpleautofishing/safMode.java
+++ src/main/java/com/minecraft/simpleautofishing/safMode.java
@@ -11,38 +11,38 @@ public class safMode {
 	//global vars
 	static int mode; 
 	static Minecraft Inst = Minecraft.getInstance();
-	
-    //check for FishingRod, sneaking and attack
-    @SubscribeEvent
-    public void ModeChanger(ClickInputEvent event) {
-    	if (Inst.player.isCrouching() && Inst.player.getHeldItemMainhand().getItem() == Items.FISHING_ROD && event.isAttack()) {
-    		if (mode == 2) {
-    			mode = 0;
-    		} else {
-    			mode++;
-    		}
-    		if (mode == 0) {
-        		Inst.player.sendMessage(new TranslationTextComponent("text.simpleautofishing.safMode.fishing_rod_unprotected"), Inst.player.getUniqueID());
-    		} else if (mode == 1) {
-        		Inst.player.sendMessage(new TranslationTextComponent("text.simpleautofishing.safMode.fishing_rod_protected"), Inst.player.getUniqueID());
-    		} else if (mode == 2) {
-        		Inst.player.sendMessage(new TranslationTextComponent("text.simpleautofishing.safMode.all_in_hotbar"), Inst.player.getUniqueID());
-    		}
-    	}
+
+	//check for FishingRod, sneaking and attack
+	@SubscribeEvent
+	public void ModeChanger(ClickInputEvent event) {
+		if (Inst.player.isCrouching() && Inst.player.getHeldItemMainhand().getItem() == Items.FISHING_ROD && event.isAttack()) {
+			if (mode == 2) {
+				mode = 0;
+			} else {
+				mode++;
+			}
+			if (mode == 0) {
+					Inst.player.sendMessage(new TranslationTextComponent("text.simpleautofishing.safMode.fishing_rod_unprotected"), Inst.player.getUniqueID());
+			} else if (mode == 1) {
+				Inst.player.sendMessage(new TranslationTextComponent("text.simpleautofishing.safMode.fishing_rod_protected"), Inst.player.getUniqueID());
+			} else if (mode == 2) {
+				Inst.player.sendMessage(new TranslationTextComponent("text.simpleautofishing.safMode.all_in_hotbar"), Inst.player.getUniqueID());
+			}
+		}
 	}
-    
+
 	//Modes
-    public static boolean Modes() {
-    	ItemStack ism = Inst.player.getHeldItemMainhand();
-    	ItemStack iso = Inst.player.getHeldItemOffhand();
-    	if (mode == 1) {
-    		if (ism.getDamage() >= ism.getMaxDamage() - 3) {
-    			return false;
-    		} else if (ism.getItem() != Items.FISHING_ROD && iso.getDamage() >= iso.getMaxDamage() - 3) {
-    			return false;
-    		}
-    	}
-    	if (mode == 2 && ism.getItem() != Items.FISHING_ROD) {
+	public static boolean Modes() {
+		ItemStack ism = Inst.player.getHeldItemMainhand();
+		ItemStack iso = Inst.player.getHeldItemOffhand();
+		if (mode == 1) {
+			if (ism.getDamage() >= ism.getMaxDamage() - 3) {
+				return false;
+			} else if (ism.getItem() != Items.FISHING_ROD && iso.getDamage() >= iso.getMaxDamage() - 3) {
+				return false;
+			}
+		}
+		if (mode == 2 && ism.getItem() != Items.FISHING_ROD) {
 			for (int i = 0; i < 9; i++) {
 				if (ism.getItem() == Items.FISHING_ROD) {
 					return true;
@@ -50,7 +50,7 @@ public class safMode {
 				Inst.player.inventory.changeCurrentItem(1D);
 			}
 			return false;
-    	}
-    	return true;
-    }        
+		}
+		return true;
+	}
 }
