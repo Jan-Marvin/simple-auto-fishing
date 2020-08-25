4125bcb2456db029f2ebaf67f913c8e047472d63
Jan-Marvin <69450869+Jan-Marvin@users.noreply.github.com>
2020-08-25 19:30:38 +0200

    Fix spacing

--- src/main/java/com/minecraft/simpleautofishing/simpleautofishing.java
+++ src/main/java/com/minecraft/simpleautofishing/simpleautofishing.java
@@ -9,15 +9,15 @@ import org.apache.logging.log4j.Logger;
 @Mod("simpleautofishing")
 public class simpleautofishing
 {
-    // Directly reference a log4j logger.
-    static final Logger LOGGER = LogManager.getLogger();
-    
+	// Directly reference a log4j logger.
+	static final Logger LOGGER = LogManager.getLogger();
+
 	public static safCore safCore = new safCore();
 	public static safMode safMode = new safMode();
-	
+
 	public simpleautofishing() {
 		
-        MinecraftForge.EVENT_BUS.register(simpleautofishing.safCore);
-        MinecraftForge.EVENT_BUS.register(simpleautofishing.safMode);
-    }
+		MinecraftForge.EVENT_BUS.register(simpleautofishing.safCore);
+		MinecraftForge.EVENT_BUS.register(simpleautofishing.safMode);
+	}
 }
