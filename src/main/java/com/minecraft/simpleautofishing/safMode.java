debe0f4fcfa7bcd684a4e9a14b58085befa98866
Jan-Marvin <69450869+Jan-Marvin@users.noreply.github.com>
2020-11-09 19:12:49 +0100

    Fix rod selector

--- src/main/java/com/minecraft/simpleautofishing/safMode.java
+++ src/main/java/com/minecraft/simpleautofishing/safMode.java
@@ -1,6 +1,7 @@
 package com.minecraft.simpleautofishing;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -33,18 +34,16 @@ public class safMode {
 
 	//Modes
 	public static boolean Modes() {
-		ItemStack ism = Inst.player.getHeldItemMainhand();
-		ItemStack iso = Inst.player.getHeldItemOffhand();
 		if (mode == 1) {
-			if (ism.getDamage() >= ism.getMaxDamage() - 3) {
+			if (Inst.player.getHeldItemMainhand().getDamage() >= Inst.player.getHeldItemMainhand().getMaxDamage() - 3) {
 				return false;
-			} else if (ism.getItem() != Items.FISHING_ROD && iso.getDamage() >= iso.getMaxDamage() - 3) {
+			} else if (Inst.player.getHeldItemMainhand().getItem() != Items.FISHING_ROD && Inst.player.getHeldItemOffhand().getDamage() >= Inst.player.getHeldItemOffhand().getMaxDamage() - 3) {
 				return false;
 			}
 		}
-		if (mode == 2 && ism.getItem() != Items.FISHING_ROD) {
-			for (int i = 0; i < 9; i++) {
-				if (ism.getItem() == Items.FISHING_ROD) {
+		if (mode == 2 && Inst.player.getHeldItemMainhand().getItem() != Items.FISHING_ROD) {
+			for (int i = 0; i < PlayerInventory.getHotbarSize(); i++) {
+				if (Inst.player.getHeldItemMainhand().getItem() == Items.FISHING_ROD) {
 					return true;
 				}
 				Inst.player.inventory.changeCurrentItem(1D);
